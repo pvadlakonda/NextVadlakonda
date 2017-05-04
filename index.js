2254e6aba542612c96b2778ff25b80ffb668be75
Praveen Vadlakonda <Praveen.Vadlakonda@aa.com>
2017-05-04 00:16:14 -0500

    Added pic chart

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
     console.log("ready!");
-    populateTable(retrieveData());
+    retrieveData();
 });
 
 function retrieveData() {
-    var responses;
     $.ajax({
         dataType: "json",
         type: "GET",
         url: "https://api.mlab.com/api/1/databases/vadlakonda/collections/comments?apiKey=2-byIVNo-oqo6Irfu3ywY1OkJW8GY_xh",
         success: function(data) {
-            responses = data;
+            populateTable(data);
         }
     });
-    return responses;
 }
 
 function submitData() {
@@ -28,9 +26,11 @@ function submitData() {
         url: "https://api.mlab.com/api/1/databases/vadlakonda/collections/comments?apiKey=2-byIVNo-oqo6Irfu3ywY1OkJW8GY_xh",
         data: JSON.stringify({ "name": name, "gender": gender, "suggestedName": suggestedName }),
         type: "POST",
-        contentType: "application/json"
+        contentType: "application/json",
+        success: function() {
+            retrieveData();
+        }
     });
-    populateTable(retrieveData());
 }
 
 function invalidData(name, gender) {
@@ -40,7 +40,43 @@ function invalidData(name, gender) {
     return false;
 }
 
+function populateChart(boyCount, girlCount) {
+    var chart = new CanvasJS.Chart("chartContainer", {
+        title: {
+            text: "What dear ones think so far",
+            fontFamily: "arial black"
+        },
+        animationEnabled: true,
+        legend: {
+            verticalAlign: "bottom",
+            horizontalAlign: "center"
+        },
+        theme: "theme1",
+        data: [{
+            type: "pie",
+            indexLabelFontFamily: "Garamond",
+            indexLabelFontSize: 20,
+            indexLabelFontWeight: "bold",
+            startAngle: 0,
+            indexLabelFontColor: "MistyRose",
+            indexLabelLineColor: "darkgrey",
+            indexLabelPlacement: "inside",
+            toolTipContent: "{name}: {y}",
+            showInLegend: true,
+            indexLabel: "#percent%",
+            dataPoints: [
+                { y: boyCount, name: "Boy", legendMarkerType: "triangle" },
+                { y: girlCount, name: "Girl", legendMarkerType: "circle" }
+            ]
+        }]
+    });
+    chart.render();
+
+}
+
 function populateTable(data) {
+    var boyCount = 0;
+    var girlCount = 0;
     if (data) {
         var len = data.length;
 
@@ -55,9 +91,15 @@ function populateTable(data) {
                 if (txt !== "") {
                     $("#table").append(txt).removeClass("hidden");
                 }
+                if (comment.gender === 'Boy') {
+                    boyCount++;
+                } else if (comment.gender === 'Girl') {
+                    girlCount++;
+                }
             }
         }
     }
+    populateChart(boyCount, girlCount);
     clearFields();
 }
 
